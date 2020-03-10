c4cbd91d1304cddda2dc11668d7e80cc89a65dca
fiqriismail <fiqri@peachit.lk>
2020-03-10 17:33:21 +0530

    Added status coloring

--- src/components/TodoList/TodoItem.jsx
+++ src/components/TodoList/TodoItem.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
+function getStatusStyle(status) {
+  switch (status) {
+    case 'In Progress':
+      return 'orange';
+    case 'Completed':
+      return 'green';
+    case 'Cancelled':
+      return 'red';
+    default:
+      break;
+  }
+}
+
 function TodoItem(props) {
   return (
-    <li>
+    <li style={{ color: getStatusStyle(props.status) }}>
       {props.title} - {props.status} - Created: {props.created} by {props.owner}
     </li>
   );
