98f3938070fb1023ae62d1c68563cb3b208875fb
fiqriismail <fiqri@peachit.lk>
2020-03-10 17:23:06 +0530

    Added props to React Component

--- src/components/TodoList/TodoItem.jsx
+++ src/components/TodoList/TodoItem.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-function TodoItem() {
-  return <li>Todo Item 1 - In Progress - Created: 10-02-2020</li>;
+function TodoItem(props) {
+  return (
+    <li>
+      {props.title} - {props.status} - Created: {props.created} by {props.owner}
+    </li>
+  );
 }
 
 export default TodoItem;
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -5,12 +5,24 @@ import TodoItem from './TodoItem';
 function TodoList() {
   return (
     <ul>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      <TodoItem
+        title="Create react app"
+        status="In Progress"
+        created="10-02-2020"
+        owner="Fiqri"
+      />
+      <TodoItem
+        title="Explain about props"
+        status="Completed"
+        created="10-02-2020"
+        owner="Fiqri"
+      />
+      <TodoItem
+        title="Show how it works"
+        status="New"
+        created="10-02-2020"
+        owner="Fiqri"
+      />
     </ul>
   );
 }
